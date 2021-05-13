ac9724b898d65e07fe5909e5584e06c79f19e63c
Ryan Apfel <ryan@MacBook-Air.local>
2021-05-13 14:01:04 -0700

    content as

--- src/data/content.py
+++ src/data/content.py
@@ -26,7 +26,7 @@ class Content(Meta):
     data = []
     for i in res:
         data.append(np.array([i[0], self.getSceneIdx(i[1]), i[2]]))
-    self.results = np.array(data)
+    self.results = np.asarray(data)
     return self.results
 
 
