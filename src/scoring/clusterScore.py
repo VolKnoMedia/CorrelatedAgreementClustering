a8fad0f9951523730e7d7eef093044eb8fa7044c
Ryan Apfel <ryan@MacBook-Air.local>
2021-05-24 09:00:01 -0700

    scoring

--- src/scoring/clusterScore.py
+++ src/scoring/clusterScore.py
@@ -127,63 +127,7 @@ class Scores:
       else:
         return 0
     return sum(scs)
-        
-
-class ScoreStruct:
-  def __init__(self, struct):
-    self.vectors = struct.vectors
-    self.emotions = struct.emotions
-    self.scores = [CorrelationStructure(self.emotions) for i in range(len(self.vectors))]
-
-
-  def __getitem__(self, attr):
-    return self.scores[attr]
-
-  def getScores(self, method='standard'):
-    if method == 'standard':
-      self.standardScoring()
-    elif method == 'threshold':
-      self.thresholdScoring()
-    elif method == 'binary':
-      self.postiveOrNegative()
-  
-  def standardScoring(self): 
-    for kidx, k in enumerate(self.vectors):
-      # k == correlationStructure
-      for ix,x in enumerate(k.map.keys()):
-        px = k.prob[k.map[x], :].sum().sum()
-        for iy,y in enumerate(k.map.keys()):
-          py = k.prob[:, k.map[y] ].sum()
-          pxy = px*py
-          jpxy = k.prob[k.map[x]][k.map[y]]
-          self.scores[kidx].vector[k.map[x]][k.map[y]] = 1 if jpxy > pxy else 0
-
-  def thresholdScoring(self, threshold = -0.001):
-    for kidx, k in enumerate(self.vectors):
-      # k == correlationStructure
-      for ix,x in enumerate(k.map.keys()):
-        px = k.prob[k.map[x]].sum()
-        for iy,y in enumerate(k.map.keys()):
-          py = k.prob[k.map[y]].sum()
-          pxy = px*py
-          jpxy = k.prob[k.map[x]][k.map[y]]
-
-          if (jpxy-pxy) > -0.001:
-            self.scores[kidx].vector[k.map[x]][k.map[y]] = 1
-          else:
-            self.scores[kidx].vector[k.map[x]][k.map[y]] = 0
-  # def postiveOrNegative(self):
-  #   for kidx, k in enumerate(self.vectors):
-  #     # k == correlationStructure
-  #     for ix,x in enumerate(k.map.keys()):
-  #       for iy,y in enumerate(k.map.keys()):\
-  #         px = k.prob[:, k.map[x]].sum()
-  #         py = k.prob[k.map[y], : ].sum()
-  #         pxy = px*py
-  #         jpxy = k.prob[k.map[x]][k.map[y]]
-
-  #         self.scores[kidx].vector[k.map[x]][k.map[y]] = 1 if jpxy > pxy else -1
-
+    
   def _getY(self, scene,  debug=False):
     othersRespondToIth = self.globalScene[scene]
     rIdx = np.random.randint(low=0, high=othersRespondToIth.shape[1])
@@ -229,3 +173,61 @@ class ScoreStruct:
         print(scene, response)
 
     return response
+        
+
+class ScoreStruct:
+  def __init__(self, struct):
+    self.vectors = struct.vectors
+    self.emotions = struct.emotions
+    self.scores = [CorrelationStructure(self.emotions) for i in range(len(self.vectors))]
+
+
+  def __getitem__(self, attr):
+    return self.scores[attr]
+
+  def getScores(self, method='standard'):
+    if method == 'standard':
+      self.standardScoring()
+    elif method == 'threshold':
+      self.thresholdScoring()
+    elif method == 'binary':
+      self.postiveOrNegative()
+  
+  def standardScoring(self): 
+    for kidx, k in enumerate(self.vectors):
+      # k == correlationStructure
+      for ix,x in enumerate(k.map.keys()):
+        px = k.prob[k.map[x], :].sum().sum()
+        for iy,y in enumerate(k.map.keys()):
+          py = k.prob[:, k.map[y] ].sum()
+          pxy = px*py
+          jpxy = k.prob[k.map[x]][k.map[y]]
+          self.scores[kidx].vector[k.map[x]][k.map[y]] = 1 if jpxy > pxy else 0
+
+  def thresholdScoring(self, threshold = -0.001):
+    for kidx, k in enumerate(self.vectors):
+      # k == correlationStructure
+      for ix,x in enumerate(k.map.keys()):
+        px = k.prob[k.map[x]].sum()
+        for iy,y in enumerate(k.map.keys()):
+          py = k.prob[k.map[y]].sum()
+          pxy = px*py
+          jpxy = k.prob[k.map[x]][k.map[y]]
+
+          if (jpxy-pxy) > -0.001:
+            self.scores[kidx].vector[k.map[x]][k.map[y]] = 1
+          else:
+            self.scores[kidx].vector[k.map[x]][k.map[y]] = 0
+
+
+  # def postiveOrNegative(self):
+  #   for kidx, k in enumerate(self.vectors):
+  #     # k == correlationStructure
+  #     for ix,x in enumerate(k.map.keys()):
+  #       for iy,y in enumerate(k.map.keys()):\
+  #         px = k.prob[:, k.map[x]].sum()
+  #         py = k.prob[k.map[y], : ].sum()
+  #         pxy = px*py
+  #         jpxy = k.prob[k.map[x]][k.map[y]]
+
+  #         self.scores[kidx].vector[k.map[x]][k.map[y]] = 1 if jpxy > pxy else -1
